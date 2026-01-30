6ea071ec35d57adf69004a4fa3213d49a942e5ae
ToniiGP <juan.tony1516@gmail.com>
2026-01-30 15:05:07 -0600

    Implemented firts version of method to display hangman

--- src/Main.java
+++ src/Main.java
@@ -15,19 +15,74 @@ public class Main
         //variable to keep count of the wrong attempts
         int wrongCount = 0;
 
-
         //inserting underscores with the lenght of the used word to display for the user
         for(int i = 0; i < word.length(); i++)
         {
             wordState.add('_');
         }
 
-        //debugging print statement
-        System.out.println(wordState);
+        System.out.println("************************");
+        System.out.println("Welcome to Java Hangman");
+        System.out.println("************************");
 
+        //testing method
+        System.out.println(getHangmanArt((6)));
 
 
         scanner.close();
 
     }
+
+
+    //getHangmanArt method gets and int as parameter and return a string
+    //is usde to display the Hangman depending in the # of attemps left
+    static String getHangmanArt(int wrongCount)
+    {
+        return switch(wrongCount)
+        {
+            case 0 -> """
+                      
+                      """;
+            case 1 -> """
+                      0
+                      
+                      
+                      """;
+
+            case 2 -> """
+                      0
+                      |
+                      
+                      
+                      """;
+
+            case 3 -> """
+                       0
+                      /|
+                      
+                      """;
+
+            case 4 -> """
+                        0
+                       /|\\
+                      
+                      """;
+
+            case 5 -> """
+                       0
+                      /|\\
+                      /
+                      """;
+
+            case 6 -> """
+                       0
+                      /|\\
+                      /\\
+                      """;
+
+            default -> "";
+        };
+
+    }
 }
+
